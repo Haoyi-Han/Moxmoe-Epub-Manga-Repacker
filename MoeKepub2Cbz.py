2051bb1c43ff478d99a497f8a1473996c404a51c
Haoyi HAN <54799247+Haoyi-Han@users.noreply.github.com>
2022-09-05 19:55:53 +0800

    Fix of some bugs

--- MoeKepub2Cbz.py
+++ MoeKepub2Cbz.py
@@ -1,8 +1,15 @@
 import os, shutil, pathlib, zipfile, re
 from bs4 import BeautifulSoup
+import signal
+from alive_progress import alive_bar
+
+# 键盘Ctrl+C中断命令优化（暂时用不到）
+def keyboardHandler(signum, frame):
+    print(f'\033[93m您手动中断了程序，已经转换的文件和缓存文件夹将保留。\033[0m')
+    exit()
 
 # 在指定目录下复制空目录结构
-def copyDirStruct(inPath, outPath, ifinclude=True):
+def copyDirStruct(inPath: str, outPath: str, ifinclude: bool=True):
     if ifinclude:
         ignore_files = lambda dir, files: [f for f in files if os.path.isfile(os.path.join(dir, f))] + [os.path.split(outPath)[1]]
     else:
@@ -10,18 +17,21 @@ def copyDirStruct(inPath, outPath, ifinclude=True):
     shutil.copytree(inPath, outPath, ignore=ignore_files)
 
 # 创建文件列表（按原目录结构）    
-def copyDirStructToList(root):
+def copyDirStructToList(root: str) -> list:
     return [pathlib.Path(os.path.join(path, name)) for path, subdirs, files in os.walk(root) for name in files]
 
 # 创建EPUB文件列表（按原目录结构）
-def copyDirStructExtToList(root, ext='.epub'):
-    filelist = copyDirStructToList(root)
+def copyDirStructExtToList(root: str, ext='.epub') -> list:
+    filelist: list = copyDirStructToList(root)
     return [p for p in filelist if p.suffix==ext]
 
 # 初始化路径并复制目录结构
-def initPathObj(output_folder_name):
+def initPathObj(output_folder_name: str) -> tuple:
     curr_path = pathlib.Path(os.getcwd())
-    curr_filelist = copyDirStructExtToList(str(curr_path))
+    cachefolder = curr_path / 'cache'
+    if cachefolder.is_dir():
+        shutil.rmtree(str(cachefolder))
+    curr_filelist: list = copyDirStructExtToList(str(curr_path))
     print("已完成文件列表抽取。")
     output_path = curr_path / 'output'
     if output_path.is_dir():
@@ -33,7 +43,8 @@ def initPathObj(output_folder_name):
     return curr_path, output_path, curr_filelist
 
 # 修改EPUB扩展名为ZIP
-def suffixChange(filelist, inType='.epub', outType='.zip'):
+# 调整shutil.unpack_archive()参数后，解压不再需要依赖扩展名，本函数弃用
+def suffixChange(filelist: list, inType: str='.epub', outType: str='.zip') -> list:
     for i in range(len(filelist)):
         filepath = filelist[i]
         if filepath.suffix == inType:
@@ -41,28 +52,35 @@ def suffixChange(filelist, inType='.epub', outType='.zip'):
         filelist[i] = filepath
     return filelist
 
+# 漫画名称抽取
+def comicNameExtract(comic_file) -> str:
+    return re.search(r'moe](.+?)\.+?', str(comic_file.stem)).group(1)
+
 # 单个压缩包根据HTML文件中的图片地址进行提取
 def loadZipImg(zip_file, cachefolder):
     print("开始解析 ", zip_file.stem)
+    # 避免相同文件名解压到缓存文件夹时冲突
     extract_dir = cachefolder / str(zip_file.stem)
-    comic_name = re.search(r'moe](.+?)\.+?', str(zip_file.stem)).group(1)
-    print("{} => 开始提取".format(comic_name))
-    shutil.unpack_archive(str(zip_file), extract_dir=extract_dir)
+    while extract_dir.is_dir():
+        extract_dir = cachefolder / (str(zip_file.stem) + '_dup')
+    comic_name: str = comicNameExtract(zip_file)
+    print(f'{comic_name} => 开始提取')
+    shutil.unpack_archive(str(zip_file), extract_dir=extract_dir, format="zip")
     html_dir = extract_dir / 'html'
     img_dir = extract_dir / 'image'
-    html_list = copyDirStructExtToList(str(html_dir), ext=".html")
-    def checkIfHtmlFileNameLegal(name_str):
+    html_list: list = copyDirStructExtToList(str(html_dir), ext=".html")
+    def checkIfHtmlFileNameLegal(name_str: str) -> bool:
         if name_str.isnumeric(): return True
         else:
             name_str = name_str.lower()
             return (('cover.jpg' in name_str) or ('createby' in name_str))
     for html_file in html_list:
-        html_file_name = html_file.stem
+        html_file_name: str = html_file.stem
         if not checkIfHtmlFileNameLegal(html_file_name):
             continue
         with html_file.open('r', encoding='utf-8') as hf:
             soup = BeautifulSoup(hf.read(), 'html.parser')
-        title = soup.title.string
+        title: str = soup.title.string
         imgsrc = pathlib.Path(soup.img['src'])
         imgsrc = img_dir / imgsrc.name
         if 'cover' in imgsrc.name:
@@ -70,38 +88,44 @@ def loadZipImg(zip_file, cachefolder):
         elif 'END' in title:
             imgsrc = imgsrc.rename(pathlib.Path(imgsrc.parent, 'THE END' + imgsrc.suffix))
         else:
-            page_num = re.search(r'\d+', title).group(0)
+            page_num: str = re.search(r'\d+', title).group(0)
             imgsrc = imgsrc.rename(pathlib.Path(imgsrc.parent, 'PAGE {:03}'.format(int(page_num)) + imgsrc.suffix))
     img_dir = img_dir.rename(pathlib.Path(img_dir.parent, comic_name))
-    print("{} => 提取完成".format(comic_name))
+    print(f'{comic_name} => 提取完成')
     return img_dir
 
 # 打包成压缩包并重命名
-def packFolder(inDir, outDir, ext='.cbz'):
-    comic_name = inDir.name
-    print("{} => 开始打包".format(comic_name))
+def packFolder(inDir: str, outDir: str, ext: str='.cbz'):
+    comic_name: str = inDir.name
+    print(f'{comic_name} => 开始打包')
     outFile = pathlib.Path(outDir, comic_name + '.zip')
     with zipfile.ZipFile(str(outFile), mode='w') as archive:
         for file_path in inDir.iterdir():
             archive.write(file_path, arcname=file_path.name)
     outFile = outFile.rename(outFile.with_suffix(ext))
-    print("{} => 打包完成".format(comic_name))
+    print(f'{comic_name} => 打包完成')
     return outFile
 
 # 主程序
 if __name__ == '__main__':
+    # 优化键盘中断命令（备用）
+    signal.signal(signal.SIGINT, keyboardHandler)
+    signal.signal(signal.SIGTERM, keyboardHandler)
+    print('============> 开始初始化程序')
     curr_path, output_path, curr_filelist = initPathObj('output')
-    print('============> 开始重命名文件')
-    curr_filelist = suffixChange(curr_filelist)
     cachefolder = curr_path / 'cache'
-    if cachefolder.is_dir():
-        shutil.rmtree(str(cachefolder))
     print('============> 开始提取图片并打包文件')
-    for file_t in curr_filelist:
-        comic_name = file_t.parents[0].name
-        comic_src = loadZipImg(file_t, cachefolder=cachefolder)
-        packFolder(comic_src, output_path / comic_name)
-    curr_filelist = suffixChange(curr_filelist, inType='.zip', outType='.epub')
+    # 进度条效果
+    with alive_bar(len(curr_filelist), dual_line=True, ctrl_c=False, title='Mox.moe') as bar:
+        for file_t in curr_filelist:
+            comic_name: str = file_t.parents[0].name
+            bar.text = comicNameExtract(file_t)
+            comic_src = loadZipImg(file_t, cachefolder=cachefolder)
+            if comic_name == curr_path.stem:
+                packFolder(comic_src, output_path)
+            else:
+                packFolder(comic_src, output_path / comic_name)
+            bar()
     print('============> 开始清理缓存文件')
     shutil.rmtree(str(cachefolder))
     print('============> 完成')
